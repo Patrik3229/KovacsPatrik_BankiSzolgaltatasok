0499521a800f2b86c9b75af1ba5e08373bc61399
Kovács Patrik <kovacspatrik130@gmail.com>
2023-10-19 08:42:43 +0200

    BankiSzolgaltatas, Szamla osztályok

---
+++ BankiSzolgaltatasok/Szamla.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+    internal class Szamla : BankiSzolgaltatas
+    {
+        protected int aktualisEgyenleg;
+
+        public Szamla(Tulajdonos tulajdonos) : base(tulajdonos)
+        {
+            
+        }
+
+        public int AktualisEgyenleg { get => aktualisEgyenleg; }
+
+        public void Befizet(int osszeg)
+        {
+            this.aktualisEgyenleg += osszeg;
+        }
+
+        public bool Kivesz(int osszeg)
+        {
+            if (this.aktualisEgyenleg >= osszeg)
+            {
+                this.aktualisEgyenleg -= osszeg;
+                return true;
+            }
+            return false;
+        }
+    }
+}
